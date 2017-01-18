dc6edcf4e170a08f073440e237c6d0bc0d881bf5
Ilia Choly <icholy@accipiterradar.com>
2017-01-18 16:38:40 -0500

    make it pip installable

---
+++ durationpy/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: UTF-8 -*-
+
+from duration import to_str, from_str
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'durationpy',
+    url          = 'https://github.com/icholy/Duration.py',
+    author       = 'Ilia Choly',
+    author_email = 'ilia.choly@gmail.com',
+    version      = '0.1',
+    packages     = ['durationpy'],
+    license      = 'MIT'
+)
