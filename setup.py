81d4690c06248b26d9428dc87a789934ae0e4a06
Ilia Choly <icholy@accipiterradar.com>
2017-01-19 10:33:51 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url          = 'https://github.com/icholy/Duration.py',
     author       = 'Ilia Choly',
     author_email = 'ilia.choly@gmail.com',
-    version      = '0.1',
+    version      = '0.2',
     packages     = ['durationpy'],
     license      = 'MIT'
 )
